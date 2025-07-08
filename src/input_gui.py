b7c7ec124bf969cb35dca3f1fdf3a3fd22271275
Alex Grazela <agrazela@gmail.com>
2025-07-08 13:49:45 -0400

    Starting work on refactor, player section first, then will copy that to the other data types if it works

--- src/input_gui.py
+++ src/input_gui.py
@@ -41,77 +41,30 @@ class Unit:
 # Callback function of the add button, located in the parent class, calls a function in the extended Player/Objective/Unit class
 # The called function there adds the new row, as well as the data stored in the child class
 
-class PlayerBox(QtWidgets.QVBoxLayout))
+class PlayerSection():
     # A list of player dataclass objects containing the entered data
-    player_data = []     
+    player_data   : list    
+    # holds the HBox layouts which contain the input form
+    player_vlayout: QtWidgets.QVBoxLayout 
     
     def __init__(self):
-        super().__init() 
+        self.player_vlayout = QtWidgets.QVBoxLayout() 
+        self.player_data    = []
 
-    def PlayerAddCallback():
-        
-
-# Tie each entry to a dataclass and store them in the AppGui class itself?
-# I don't love it but might be the easiest way to get the data from here to the pdf maker
-
-class AppGui(QtWidgets.QWidget):
-    def __init__(self):
-        super().__init__()
-        
-        # Metadata initialization
-        # Add a member of the associated dataclass to these that stores their data when the GUI is interacted with
-        self.player_list    = []
-        self.objective_list = []
-        self.unit_list      = []
-        
-        # Graphical Objects Initialization
-        self.parent_layout     = QtWidgets.QVBoxLayout(self)
-        self.players_layout    = QtWidgets.QVBoxLayout()
-        self.objectives_layout = QtWidgets.QVBoxLayout()
-        self.units_layout      = QtWidgets.QVBoxLayout()
-        self.make_pdf_button   = QtWidgets.QPushButton("Make PDF")
-
-        self.parent_layout.addLayout(self.players_layout)
-        self.parent_layout.addLayout(self.objectives_layout)
-        self.parent_layout.addLayout(self.units_layout)
-        self.parent_layout.addWidget(self.make_pdf_button)
-        # Create the parent widget, including a VBoxLayout that holds the other layouts
-        # addLayout to add a Players, Objectives, Units, Scoring, HBoxLayouts
-
-
-        # Include an Add button to add a new unit, each unit is represented by text boxes
-        # [Unit Name, Unit BV, Unit Skill] for each unit
-        self.add_player_button = QtWidgets.QPushButton("Add Player")
-        self.add_player_button.clicked.connect(self.add_player)
-        self.players_layout.addWidget(self.add_player_button)
-
-        self.add_objective_button = QtWidgets.QPushButton("Add Objective")
-        self.add_objective_button.clicked.connect(self.add_objective)
-        self.objectives_layout.addWidget(self.add_objective_button)
-
-        self.add_unit_button = QtWidgets.QPushButton("Add Unit")
-        self.add_unit_button.clicked.connect(self.add_unit)
-        self.units_layout.addWidget(self.add_unit_button)
-
-        self.make_pdf_button.clicked.connect(self.make_pdf)
-
-
-
-    # TODO: make the Player number a dropdown with numbers
-    #       The layout containing the dropdown and it's label has way too much spacing between the two
-    def add_player(self): 
+    def AddPlayerCallback():
+        # Called by the parent class's callback, adds the HBox and data to player_vlayout and player_data respectively
         new_player            = QtWidgets.QHBoxLayout()
         player_number_layout  = QtWidgets.QHBoxLayout()
         player_number_counter = QtWidgets.QComboBox()
         player_name           = QtWidgets.QLineEdit()
         player_faction        = QtWidgets.QLineEdit()
+        player_data           = PlayerData()
 
         player_number_counter.addItems(["1", "2", "3", "4", "5", "6", "7", "8", "9"])
         player_number_counter.setFrame( True )
 
         player_number_layout.addWidget(QtWidgets.QLabel("Player Number"))
         player_number_layout.addWidget(player_number_counter)
-        player_number_layout.setSizeConstraint(QtWidgets.QLayout.SizeConstraint.SetMinimumSize)
 
         player_name.setPlaceholderText("Player Name")
         player_faction.setPlaceholderText("Player Faction")
@@ -120,62 +73,105 @@ class AppGui(QtWidgets.QWidget):
         new_player.addWidget(player_faction)
         new_player.addLayout(player_number_layout)
 
-        self.players_layout.addLayout(new_player)
+        self.player_vlayout.addLayout(new_player)
+        self.player_data.extend(player_data)
+    
+    def getPlayerLayout(self):
+        return self.player_vlayout
+         
+class AppGui(QtWidgets.QWidget):
+    parent_layout        : QtWidgets.QVBoxLayout
+    players              : PlayerSection
+    add_player_button    : QtWidgets.QPushButton
+    add_objective_button : QtWidgets.QPushButton
+    add_unit_button      : QtWidgets.QPushButton
+    make_pdf_button      : QtWidgets.QPushButton
 
-    def add_objective(self): 
-        new_objective   = QtWidgets.QHBoxLayout()
-        objective_name  = QtWidgets.QLineEdit()
-        objective_type  = QtWidgets.QLineEdit()
-        objective_value = QtWidgets.QLineEdit()
+    def __init__(self):
+        super().__init__()
+        # Graphical Objects Initialization
+        self.parent_layout = QtWidgets.QVBoxLayout()
+        
+        self.players    = PlayerSection()
+        #objectives = ObjectiveSection()
+        #units      = UnitSection()
 
-        objective_name.setPlaceholderText("Objective Name")
-        objective_type.setPlaceholderText("Objective Type (Primary, Secondary, etc...")
-        objective_value.setPlaceholderText("Objective Value")
+        self.add_player_button    = QtWidgets.QPushButton("Add Player")
+        self.add_objective_button = QtWidgets.QPushButton("Add Objective")
+        self.add_unit_button      = QtWidgets.QPushButton("Add Unit")
+        self.make_pdf_button      = QtWidgets.QPushButton("Make PDF")
+        
+        self.parent_layout.addWidget(self.add_player_button) 
+        self.parent_layout.addLayout(self.players.getPlayerLayout()) 
 
-        new_objective.addWidget(objective_name)
-        new_objective.addWidget(objective_type)
-        new_objective.addWidget(objective_value)
+        self.parent_layout.addWidget(self.add_objective_button) 
+        #parent_layout.addLayout(objectives.getObjectiveLayout()) 
 
-        self.objectives_layout.addLayout(new_objective)
+        self.parent_layout.addWidget(self.add_unit_button) 
+        #parent_layout.addLayout(units.getUnitLayout()) 
 
-    # TODO: make the owning player a dropdown with numbers
-    def add_unit(self):
-        new_unit             = QtWidgets.QHBoxLayout()
-        owner_number_layout  = QtWidgets.QHBoxLayout()
-        owner_number_counter = QtWidgets.QComboBox()
-        unit_name            = QtWidgets.QLineEdit()
-        unit_bv              = QtWidgets.QLineEdit()
-        unit_skill_layout    = QtWidgets.QHBoxLayout()
-        unit_gunnery         = QtWidgets.QComboBox()
-        unit_piloting        = QtWidgets.QComboBox()
+        self.parent_layout.addWidget(self.make_pdf_button)
+        
+        self.add_player_button.clicked.connect(self.add_player)
+        #add_objective_button.clicked.connect(add_objective)
+        #add_unit_button.clicked.connect(add_unit)
 
+    def add_player(self): 
+        self.players.AddPlayerCallback()
 
-        owner_number_counter.addItems(["1", "2", "3", "4", "5", "6", "7", "8", "9"])
-        owner_number_counter.setFrame( True )
-
-        owner_number_layout.addWidget(QtWidgets.QLabel("Owner Number"))
-        owner_number_layout.addWidget(owner_number_counter)
-
-        unit_name.setPlaceholderText("Unit Name")
-        unit_bv.setPlaceholderText("Unit BV")
-
-        unit_gunnery.addItems(["0", "1", "2", "3", "4", "5", "6", "7", "8"])
-        unit_gunnery.setCurrentIndex(4)
-
-        unit_piloting.addItems(["0", "1", "2", "3", "4", "5", "6", "7", "8"])
-        unit_piloting.setCurrentIndex(5)
-
-        unit_skill_layout.addWidget(QtWidgets.QLabel("Gunnery"))
-        unit_skill_layout.addWidget(unit_gunnery)
-        unit_skill_layout.addWidget(QtWidgets.QLabel("Piloting"))
-        unit_skill_layout.addWidget(unit_piloting)
-
-        new_unit.addWidget(unit_name)
-        new_unit.addWidget(unit_bv)
-        new_unit.addLayout(unit_skill_layout)
-        new_unit.addLayout(owner_number_layout)
-
-        self.units_layout.addLayout(new_unit)
+#    def add_objective(self): 
+#        new_objective   = QtWidgets.QHBoxLayout()
+#        objective_name  = QtWidgets.QLineEdit()
+#        objective_type  = QtWidgets.QLineEdit()
+#        objective_value = QtWidgets.QLineEdit()
+#
+#        objective_name.setPlaceholderText("Objective Name")
+#        objective_type.setPlaceholderText("Objective Type (Primary, Secondary, etc...")
+#        objective_value.setPlaceholderText("Objective Value")
+#
+#        new_objective.addWidget(objective_name)
+#        new_objective.addWidget(objective_type)
+#        new_objective.addWidget(objective_value)
+#
+#        self.objectives_layout.addLayout(new_objective)
+#
+#    def add_unit(self):
+#        new_unit             = QtWidgets.QHBoxLayout()
+#        owner_number_layout  = QtWidgets.QHBoxLayout()
+#        owner_number_counter = QtWidgets.QComboBox()
+#        unit_name            = QtWidgets.QLineEdit()
+#        unit_bv              = QtWidgets.QLineEdit()
+#        unit_skill_layout    = QtWidgets.QHBoxLayout()
+#        unit_gunnery         = QtWidgets.QComboBox()
+#        unit_piloting        = QtWidgets.QComboBox()
+#
+#
+#        owner_number_counter.addItems(["1", "2", "3", "4", "5", "6", "7", "8", "9"])
+#        owner_number_counter.setFrame( True )
+#
+#        owner_number_layout.addWidget(QtWidgets.QLabel("Owner Number"))
+#        owner_number_layout.addWidget(owner_number_counter)
+#
+#        unit_name.setPlaceholderText("Unit Name")
+#        unit_bv.setPlaceholderText("Unit BV")
+#
+#        unit_gunnery.addItems(["0", "1", "2", "3", "4", "5", "6", "7", "8"])
+#        unit_gunnery.setCurrentIndex(4)
+#
+#        unit_piloting.addItems(["0", "1", "2", "3", "4", "5", "6", "7", "8"])
+#        unit_piloting.setCurrentIndex(5)
+#
+#        unit_skill_layout.addWidget(QtWidgets.QLabel("Gunnery"))
+#        unit_skill_layout.addWidget(unit_gunnery)
+#        unit_skill_layout.addWidget(QtWidgets.QLabel("Piloting"))
+#        unit_skill_layout.addWidget(unit_piloting)
+#
+#        new_unit.addWidget(unit_name)
+#        new_unit.addWidget(unit_bv)
+#        new_unit.addLayout(unit_skill_layout)
+#        new_unit.addLayout(owner_number_layout)
+#
+#        self.units_layout.addLayout(new_unit)
 
     # Return a datastructure containing the information entered in the fields above
     # Probably a list of dataclasses for each of the items (player, objective, unit)
